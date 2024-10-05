392c94f0108f03cca4d51c130f029ac58d0694de
Naman Jain <103953760+namanmodi65@users.noreply.github.com>
2024-10-05 23:14:39 +0530

    Toast added

--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -3,29 +3,69 @@ import { useState } from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
 import {v4 as uuidv4} from 'uuid';
+import toast, { Toaster } from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+    const navigate = useNavigate();
     const [roomId, setRoomId] = useState('')
     const [userName, setUserName] = useState('')
     const createNewRoom=(e)=>{
         e.preventDefault();
         const id = uuidv4()
         setRoomId(id)
-        console.log(id)
+        // console.log(id)
+        toast.success("Created a new room")
+      }
+      
+      const joinRoom=(e)=>{
+      e.preventDefault();
+      if(roomId === "" || userName === ""){
+        toast.error("Room Id and username is required")
+        return;
+      }
+        navigate(`/editor/${roomId}`, {
+          state: { userName }
+        });
+    }
+
+    const handleInputEnter=(e)=>{
+      // console.log(e.code)
+      if(e.code === "Enter"){
+        joinRoom();
+        return ;
+      }
     }
 
   return (
     <>
+    <div>
+      <Toaster position="top-right" toastOptions={
+        {
+        success:{
+          theme:{
+            primary: "#4aed88"
+          }
+        }
+        ,
+        error:{
+          theme:{
+            primary: "#4aed88"
+          }
+        }
+        }
+      } ></Toaster>
+    </div>
     <div className="container">
         <h1 className="name">CodeStream</h1>
         <form id="connectForm">
             <label for="roomId">Room ID:</label>
-            <input type="text" onChange={(e)=> setRoomId(e.target.value)} id="roomId" name="roomId" value={roomId} required placeholder="Enter Room ID"/>
+            <input type="text" onChange={(e)=> setRoomId(e.target.value)} id="roomId" name="roomId" value={roomId}  placeholder="Enter Room ID" onKeyUp={handleInputEnter} />
 
             <label for="userName">User Name:</label>
-            <input type="text" onChange={(e)=> setUserName(e.target.value)} id="userName" name="userName" value={userName} required placeholder="Enter Your Name"/>
+            <input type="text" onChange={(e)=> setUserName(e.target.value)} id="userName" name="userName" value={userName}  placeholder="Enter Your Name" onKeyUp={handleInputEnter}/>
 
-            <button type="submit">Connect</button>
+            <button  onClick={joinRoom} >Connect</button>
 
             <p>If you don't have invite then create <Link to='/' className='link' onClick={createNewRoom}> new room </Link></p>
         </form>
